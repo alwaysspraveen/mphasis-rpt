4b1e70401f90d8c6bbd41e96639d5514d966e36b
praveenkumar75566 <praveenkumar75566@gmail.com>
2025-04-14 20:00:40 +0000

    form commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { SearchComponent } from "./search/search.component";
+import { FormComponent } from "./form/form.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, SearchComponent, FormComponent]
 })
 export class AppComponent {
   title = 'myapp';
