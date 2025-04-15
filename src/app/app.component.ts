75f41e020134233d904446e7386fa98f29974108
praveenkumar75566 <praveenkumar75566@gmail.com>
2025-04-15 15:37:14 +0000

    Latest update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { HeaderComponent } from "./header/header.component";
 import { SearchComponent } from "./search/search.component";
 import { FormComponent } from "./form/form.component";
 import { MonthlyContribComponent } from "./monthly-contrib/monthly-contrib.component";
+import { ButtonComponent } from "./button/button.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, SearchComponent, FormComponent, MonthlyContribComponent]
+  imports: [HeaderComponent, SearchComponent, FormComponent, MonthlyContribComponent, ButtonComponent]
 })
 export class AppComponent {
   title = 'myapp';
--- src/app/monthly-contrib/monthly-contrib.component.ts
+++ src/app/monthly-contrib/monthly-contrib.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { AdjustmentComponent } from "../adjustment/adjustment.component";
 
 @Component({
   selector: 'app-monthly-contrib',
-  imports: [],
+  imports: [AdjustmentComponent],
   templateUrl: './monthly-contrib.component.html',
   styleUrl: './monthly-contrib.component.css'
 })
