e4d659d835fa917bbfbb6d1dc14b52695eb6bad7
praveenkumar75566 <praveenkumar75566@gmail.com>
2025-04-20 10:23:29 +0000

    chart auto update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,16 @@ import { SearchComponent } from "./search/search.component";
 import { FormComponent } from "./form/form.component";
 import { MonthlyContribComponent } from "./monthly-contrib/monthly-contrib.component";
 import { ButtonComponent } from "./button/button.component";
-import { RetirementChartComponent } from "./chart/chart.component";
+import { ChartComponent } from "./chart/chart.component";
 import { AdjustmentComponent } from "./adjustment/adjustment.component";
+import { TotalAmountComponent } from "./total-amount/total-amount.component";
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, SearchComponent, FormComponent, MonthlyContribComponent, ButtonComponent, RetirementChartComponent,AdjustmentComponent]
+  imports: [HeaderComponent, SearchComponent, FormComponent, MonthlyContribComponent, ButtonComponent, AdjustmentComponent, TotalAmountComponent, ChartComponent]
 })
 export class AppComponent {
   title = 'myapp';
