f37dd3d956b8a2b6a153778f447ee08e334666d8
praveenkumar75566 <praveenkumar75566@gmail.com>
2025-04-28 09:21:08 +0000

    mail

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-loginpage',
-  imports: [CommonModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
